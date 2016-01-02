167039ce4c460e7cc6c84611d32aaac313b7d3c3
Douglas Smith <doug.smith34324@gmail.com>
2016-01-02 10:47:09 -0600

    add basic functionality

--- excelGui.py
+++ excelGui.py
@@ -1,35 +1,55 @@
 #!/usr/bin/python
 import tkinter
+import tkinter.filedialog
+import tkinter.messagebox
+import xlrd
+import xlwt
 
-
-def test():
-    test = tkinter.Toplevel(mainWindow)
-    button = tkinter.Button(test, text="test")
-    button.pack()
+# vars
+# todo
 
 def aboutWin():
     infoWin = tkinter.Toplevel(mainWindow)
-    infoWin.minsize(width=350,height=180)
-    infoWin.maxsize(width=350,height=180)
-    info = tkinter.Text(infoWin)
+    infoWin.minsize(width=480,height=80)
+    infoWin.maxsize(width=480,height=80)
+    info = tkinter.Label(infoWin, wraplength=0, pady=3, padx=3,
+                         text="This program allows the user to create/sort an existing excel file.\n\n"
+                              "Created by Douglas Smith.\n"
+                              "To see more of my projects check out smith7320 on Github.")
 
-    info.insert(tkinter.INSERT, "this is a test message")
     info.pack()
 
 
+def openFileWin():
+    openWin = tkinter.filedialog.askopenfilename(initialdir="C:/", title="Choose an existing Excel file",
+                                              filetypes=(("Excel 1997-2003 files","*.xls"),
+                                                         ("Excel 2007 files", "*.xlsx"),
+                                                         ("all files","*.*")))
+
+
+def saveAsFileWin():
+    saveAsWin = tkinter.filedialog.asksaveasfilename(initialdir="C:/", title="Save Excel file",
+                                              filetypes=(("Excel 1997-2003 files","*.xls"),
+                                                         ("Excel 2007 files", "*.xlsx")))
+
+
 mainWindow = tkinter.Tk()
+mainWindow.title("Excel Sorter")
 menuBar = tkinter.Menu(mainWindow)
 
+# add menu options
 fileMenu = tkinter.Menu(menuBar, tearoff=0)
-fileMenu.add_command(label="New", command=test)
-fileMenu.add_command(label="Open", command=test)
-fileMenu.add_command(label="Save", command=test)
-fileMenu.add_command(label="Save As", command=test)
+fileMenu.add_command(label="Open", command=openFileWin)
+fileMenu.add_command(label="Save As", command=saveAsFileWin)
 
 aboutMenu = tkinter.Menu(menuBar, tearoff=0)
 
 menuBar.add_cascade(label="File", menu=fileMenu)
 menuBar.add_cascade(label="About", command=aboutWin)
 
+# add window options
+
+
+# organize window
 mainWindow.config(menu=menuBar)
 mainWindow.mainloop()
